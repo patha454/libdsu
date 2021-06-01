d9c581216fff8b235129e8e023c03f1f4b8cb6e9
H Paterson <26947190+patha454@users.noreply.github.com>
2021-06-01 23:28:21 +1200

    WIP: Socket interface.

--- main.c
+++ main.c
@@ -1,10 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <fcntl.h>
+
+struct DsuServer
+{
+    int socketFd,
+    char[32] socketName,
+};
+
+void DsuInitServer(struct DsuServer* server)
+{
+    int socketFd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (socketFd == -1) {
+        perror("Create server socket: ");
+        exit(EXIT_FAILURE);
+    }
+    char name[108];
+    sprintf(name, "%d_dsu", getpid());
+    printf("%s\n", name);
+    struct sockaddr_un address;
+    address.sun_family = AF_UNIX;
+    strcpy(address.sun_path, name);
+    int status = bind(socketFd, (struct sockaddr*) &address, sizeof address);
+    if (status == -1) {
+        perror("Bind server socket: ");
+        exit(EXIT_FAILURE);
+    }
+    status = listen(socketFd, 1);
+    if (status == -1) {
+        perror("Listen to server socket: ");
+        exit(EXIT_FAILURE);
+    }
+    shutdown(socketFd, SHUT_RDWR);
+    close(socketFd);
+    unlink(name);
+}
+
 int
 main(int argc, char** argv)
 {
-  printf("Hello, world");
-  printf("%s, %s", argv[1], argv[2]);
+  struct DsuServer server;
+  DsuInitServer(&server);
   return EXIT_SUCCESS;
 }
