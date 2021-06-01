cae75ffdba95b5d88dabbb9224738b924428dc8f
H Paterson <26947190+patha454@users.noreply.github.com>
2021-06-01 18:09:28 +1200

    Feature: Setup clang-format.

--- main.c
+++ main.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void main(void) {
-    printf("Hello, world");
+int
+main(int argc, char** argv)
+{
+  printf("Hello, world");
+  printf("%s, %s", argv[1], argv[2]);
+  return EXIT_SUCCESS;
 }
