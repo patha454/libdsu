6e77917911b92c3ea2f919a8ec8de81d06ab0b20
H Paterson <26947190+patha454@users.noreply.github.com>
2021-06-09 18:39:59 +1200

    Feature: Skeleton client.

---
+++ client/client.c
@@ -0,0 +1,16 @@
+/**
+ * \file client.c
+ *
+ * \brief DSU client tool to work with libdsu libraries.
+ *
+ * \author H Paterson <harley.paterson@postgrad.otago.ac.nz>
+ * \copyright BSL-1.0.
+ */
+
+#include <stdlib.h>
+
+int
+main(void)
+{
+  return EXIT_SUCCESS;
+}
